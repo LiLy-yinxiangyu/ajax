ef14e70ef8309524ee57cec805bae6306edaf547
LiLy-yinxiangyu <1049725739@qq.com>
2019-09-26 19:43:49 +0800

    自己的第一个工程

---
+++ src/test/java/com/zpark/ajax7/Ajax7ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zpark.ajax7;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ajax7ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
